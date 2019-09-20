d00c773f51d9f0c57d901ee84d206f3213c719b2
bob <bob@sivercrowlabsl.com>
2019-09-19 19:26:55 -0500

    update artisan make:model for app/Model by default

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,27 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Console\Commands\ModelMakeCommand;
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+class AppServiceProvider extends ServiceProvider {
 
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
-    public function boot()
-    {
+    public function boot() {
         //
     }
+
+	/**
+	* Register any application services.
+	*
+	* @return void
+	*/
+	public function register() {
+	$this->app->extend('command.model.make', function ($command, $app) {
+		return new ModelMakeCommand($app['files']);
+	});
+	}
 }
